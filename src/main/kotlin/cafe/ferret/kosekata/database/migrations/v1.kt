c5c53a5f670ffeecfca57b5a7b6f8802fb1fe630
Mazzie <mazziechai@gmail.com>
2023-03-03 19:33:07 -0800

    Add indexes to db

--- src/main/kotlin/cafe/ferret/kosekata/database/migrations/v1.kt
+++ src/main/kotlin/cafe/ferret/kosekata/database/migrations/v1.kt
@@ -5,8 +5,15 @@
 package cafe.ferret.kosekata.database.migrations
 
 import cafe.ferret.kosekata.database.collections.NoteCollection
+import cafe.ferret.kosekata.database.entities.Note
 import org.litote.kmongo.coroutine.CoroutineDatabase
 
 suspend fun v1(database: CoroutineDatabase) {
     database.createCollection(NoteCollection.name)
+
+    val noteCollection = database.getCollection<Note>(NoteCollection.name)
+    // Guild index
+    noteCollection.ensureIndex(Note::guild)
+    // User index
+    noteCollection.ensureIndex(Note::author)
 }
