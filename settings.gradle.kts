96df360d4033c14000f87548769d1a9821fa3bd9
Mazzie <mazziechai@protonmail.com>
2024-11-30 16:06:16 -0800

    chore: update KordEx plugin, Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ pluginManagement {
 
         id("com.github.johnrengelman.shadow") version "8.1.1"
 
-        id("dev.kordex.gradle.kordex") version "1.5.4"
+        id("dev.kordex.gradle.kordex") version "1.6.0"
     }
 
     repositories {
