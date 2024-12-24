5216db898a318ff1102b8785c3b26ff7f21a142b
Mazzie <mazziechai@protonmail.com>
2024-12-23 17:55:02 -0800

    feat: scuffed reaction-based removal of note views

--- src/main/kotlin/cafe/ferret/kosekata/extensions/ChatCommandsExtension.kt
+++ src/main/kotlin/cafe/ferret/kosekata/extensions/ChatCommandsExtension.kt
@@ -6,11 +6,16 @@ package cafe.ferret.kosekata.extensions
 
 import cafe.ferret.kosekata.database.collections.NoteCollection
 import cafe.ferret.kosekata.i18n.Translations
+import dev.kord.common.entity.Permission
+import dev.kord.core.behavior.edit
+import dev.kord.core.event.message.ReactionAddEvent
 import dev.kordex.core.checks.anyGuild
 import dev.kordex.core.commands.Arguments
 import dev.kordex.core.commands.converters.impl.string
 import dev.kordex.core.extensions.Extension
 import dev.kordex.core.extensions.chatCommand
+import dev.kordex.core.extensions.event
+import dev.kordex.core.utils.hasPermission
 import dev.kordex.core.utils.respond
 import org.koin.core.component.inject
 
@@ -52,6 +57,33 @@ class ChatCommandsExtension : Extension() {
                     content = "${note.content}\n\n`#%06x` `%s`".format(note._id, note.name)
                     if (references.any()) {
                         content += "\nThis note contains note references, which are only available in the slash command equivalents of this command."
+                    }
+                }
+            }
+
+            event<ReactionAddEvent> {
+                check {
+                    val interaction = event.getMessage().interaction
+                    failIf(event.guild == null)
+                    failIf(event.messageAuthorId != bot.kordRef.selfId)
+                    failIf(event.emoji.name != "❌")
+                    if (interaction == null) {
+                        failIf(
+                            event.userId != event.getMessage().referencedMessage?.author?.id || event.getUserAsMember()
+                                ?.hasPermission(Permission.ManageMessages) == false
+                        )
+                    } else {
+                        failIfNot(interaction.name in listOf("post", "peek", "postid", "peekid"))
+                        failIf(
+                            event.userId != interaction.user.id || event.getUserAsMember()
+                                ?.hasPermission(Permission.ManageMessages) == false
+                        )
+                    }
+                }
+
+                action {
+                    event.message.edit {
+                        content = "<removed>"
                     }
                 }
             }
