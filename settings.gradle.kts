991c7c59aeb96155e24160ff16b2b26f9423db0c
Mazzie <mazziechai@protonmail.com>
2025-01-20 17:31:48 -0800

    chore: update Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,8 @@
 pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
-        kotlin("jvm") version "2.0.21"
-        kotlin("plugin.serialization") version "2.0.21"
+        kotlin("jvm") version "2.1.0"
+        kotlin("plugin.serialization") version "2.1.0"
 
         id("com.github.johnrengelman.shadow") version "8.1.1"
 
